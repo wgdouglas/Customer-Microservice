7b23ed8d319357f9c0d326445a8d2732329e8b5d
gfizzle <winstongdouglas4@gmail.com>
2023-06-04 18:46:41 -0500

    Customer-Microservice

---
+++ customer/src/main/java/com/wgcodecompany/customer/Customer.java
@@ -0,0 +1,11 @@
+package com.wgcodecompany.customer;
+
+//everySub
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Customer { //this class is the model for our microservice
+
+}
