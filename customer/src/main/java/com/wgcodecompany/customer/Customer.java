6b2a13bcfb421045b055cc5ece6e2b892def297f
gfizzle <winstongdouglas4@gmail.com>
2023-06-04 18:53:27 -0500

    Customer-Microservice

--- customer/src/main/java/com/wgcodecompany/customer/Customer.java
+++ customer/src/main/java/com/wgcodecompany/customer/Customer.java
@@ -8,4 +8,9 @@ import lombok.Data;
 @Builder
 public class Customer { //this class is the model for our microservice
 
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
 }
