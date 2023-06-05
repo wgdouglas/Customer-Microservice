05eac3ad143bdcfec166bac205835d307b269d66
Wg Douglas II <winstongdouglas4@gmail.com>
2023-06-04 19:02:27 -0500

    Merge pull request #1
    * Customer-Microservice
    
    * Customer-Microservice

---
+++ customer/src/main/java/com/wgcodecompany/customer/Customer.java
@@ -0,0 +1,16 @@
+package com.wgcodecompany.customer;
+
+//everySub
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Customer { //this class is the model for our microservice
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
