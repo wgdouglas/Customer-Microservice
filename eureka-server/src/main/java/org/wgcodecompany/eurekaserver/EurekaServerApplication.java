cb882cc48f9ecaa303663af877a8aef74edda0a5
gfizzle <winstongdouglas4@gmail.com>
2023-11-11 19:37:40 -0600

    Service discovery & load balancing with eureka server with multiple instances

---
+++ eureka-server/src/main/java/org/wgcodecompany/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package org.wgcodecompany.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
