75226a867d34d8b52d8337f0fe73a9e41c8fa003
gfizzle <winstongdouglas4@gmail.com>
2023-11-11 20:03:35 -0600

    Eureka server load balancing annotation added for handling requests & updated service call path

--- customer/src/main/java/com/wgcodecompany/customer/CustomerService.java
+++ customer/src/main/java/com/wgcodecompany/customer/CustomerService.java
@@ -25,7 +25,7 @@ public class CustomerService { //the service will handle the Post request
 
         //todo: check if fraudster
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
